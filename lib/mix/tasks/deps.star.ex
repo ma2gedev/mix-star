7b23f54032f5dbcdc513c8a032f621f9e6ecd734
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-12-18 21:38:59 +0900

    get github lists

--- lib/mix/tasks/deps.star.ex
+++ lib/mix/tasks/deps.star.ex
@@ -1,4 +1,4 @@
-defmodule Mix.Tasks.Deps.Get do
+defmodule Mix.Tasks.Deps.Star do
   use Mix.Task
 
   @shortdoc "Get dependencies with starring"
@@ -6,9 +6,19 @@ defmodule Mix.Tasks.Deps.Get do
   @moduledoc """
   """
 
-  import Mix.Deps, only: [:loaded]
+  import Mix.Deps, only: [loaded: 0]
 
   def run(args) do
     Mix.Task.run("deps.get", args)
+
+    gh_list = Enum.map loaded, fn(Mix.Dep[scm: scm, opts: opts] = deps) ->
+      match_list = Regex.named_captures(%r/^git:\/\/github.com\/(?<gh>.*).git$/g, opts[:git])
+      match_list[:gh]
+    end
+    gh_list
+      |> Enum.filter(&(&1 != nil))
+      |> Enum.map fn(gh) ->
+        IO.puts gh
+      end
   end
 end
