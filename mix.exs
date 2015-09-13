1f6486a34a87f285d8c64e8bdbb1234922f3e9f6
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-09-13 18:04:20 +0900

    bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixStar.Mixfile do
   def project do
     [ app: :mixstar,
       name: "MixStar",
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.0.0",
       package: [
         contributors: ["Takayuki Matsubara"],
